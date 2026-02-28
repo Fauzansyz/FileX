a871d792d0d7a76de5e44d520e67949d635f7372
Fauzansyz <fauzanadzma2@gmail.com>
2026-02-28 17:24:37 +0000

    Update #6

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,14 @@
 #include <ncurses.h>
 
 int main() {
-  bool inEditorPrompt = false;
     FileManager fm;
     UI ui;
 
     ui.init();
 
     while (true) {
-       
-        if (!inEditorPrompt) {
-        clear();
         ui.draw(fm.getItems(), fm.selectedIndex,fm.offset, fm.getCurrentPath().string());
-        refresh();
-        }
+
         int ch = ui.handleInput();
 
         if (ch == 'q' || ch == 'Q') break;
@@ -33,11 +28,10 @@ int main() {
             fm.selectedIndex++;
 
         if (ch == 10){
-          inEditorPrompt = true;
             auto result = fm.goToSelected();
+
             if(result == FileManager::OpenResult::OpenedFile){
               fm.openFileWithEditor(fm.getItems()[fm.selectedIndex].path().string());
-              inEditorPrompt = false;
               std::string file_path = fm.getItems()[fm.selectedIndex].path().string();
               fm.loadDirectory();
             }
